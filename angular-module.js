2d8f71fa64a473363bc59d7dd313e51060a0054e
Terrence Howard <chemisus@gmail.com>
2014-03-28 01:55:11 -0400

    Update angular-module.js
    highjacks angular.module

--- angular-module.js
+++ angular-module.js
@@ -1,60 +1,39 @@
-
-
-
-
-function module(app, requires) {
-  console.log(arguments);
+(function () {
+  module.module = angular.module;
   
-  if (!(app in module.modules)) {
-    module.modules[app] = {
-      providers: [],
-    };
+  function module(app, requires) {
+    console.log(arguments);
     
-    var methods = Object.keys(angular.module('someSuperObscureModuleName', []));
-
-    methods.map(function (method) {
-      module.modules[app][method] = function () {
-        this.providers.push([method, arguments]);
-        
-        return this;
+    if (!(app in module.modules)) {
+      module.modules[app] = {
+        providers: [],
       };
-    });
+      
+      var methods = Object.keys(module.module('someSuperObscureModuleName', []));
+  
+      methods.map(function (method) {
+        module.modules[app][method] = function () {
+          this.providers.push([method, arguments]);
+          
+          return this;
+        };
+      });
+    }
+    
+    if (arguments.length === 2) {
+      var actual = module.module(app, requires);
+      
+      module.modules[app].providers.map(function (provider) {
+        actual[provider[0]].apply(null, provider[1])
+      });
+      
+      module.modules[app] = actual;
+    }
+    
+    return module.modules[app];
   }
   
-  if (arguments.length === 2) {
-    var actual = angular.module(app, requires);
-    
-    module.modules[app].providers.map(function (provider) {
-      actual[provider[0]].apply(null, provider[1])
-    });
-    
-    module.modules[app] = actual;
-  }
+  angular.module = module;
   
-  return module.modules[app];
-}
-
-
-module.modules = {};
-
-module('App')
-  .value('a', 'A')
-  .value('b', 'B');
-
-module('App')
-   .filter('filter', function () {
-     
-   });
-
-module('App', [])
-  .value('c', 'C')
-  .value('d', 'D');
-
-module('App')
-  .value('e', 'E')
-  .value('f', 'F');
-
-module('App')
-  .run(function (a, b, c, d, e, f) {
-    console.log(a, b, c, d, e, f);
-  });
+  module.modules = {};
+})();
